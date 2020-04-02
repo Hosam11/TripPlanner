99e02ded08430c1aa2c4cc43994372846a1e7d0c
nahla samir <nahla.s1995@gmail.com>
2020-04-02 00:28:29 -0700

    maps

---
+++ app/src/main/java/com/example/trioplanner/TripsMapActivity.java
@@ -0,0 +1,40 @@
+package com.example.trioplanner;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class TripsMapActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //add lines and markers and move camera
+        // Add a marker in Sydney and move the camera
+
+        /*LatLng trip = new LatLng(-34, 151);
+        trip.
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addPolyline()
+       // mMap.setMaxZoomPreference((float) 1.5); */
+    }
+}
