4137cde6fafd45f0921a3f8f4198b00d27bbb96b
salahkamal <salahkamalpro96@gmail.com>
2020-04-02 02:15:49 +0200

    Google places autocomplete api added

--- app/src/main/java/com/example/trioplanner/AddTrip.java
+++ app/src/main/java/com/example/trioplanner/AddTrip.java
@@ -1,12 +1,10 @@
 package com.example.trioplanner;
 
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.AlarmManager;
-
 import android.app.DatePickerDialog;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
@@ -21,24 +19,28 @@ import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.trioplanner.FirebaseDBOperation.FirebaseModelImpl;
 import com.example.trioplanner.FirebaseDBOperation.HomeContract;
 import com.example.trioplanner.FirebaseDBOperation.HomePresenterImpl;
 import com.example.trioplanner.data.Trip;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Arrays;
 import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-
-
-import static com.example.trioplanner.Uitiles.SAVED_OFFLINE;
-import static com.example.trioplanner.Uitiles.SAVED_ONLINE;
-import static com.example.trioplanner.Uitiles.checkInternetState;
 
 
 public class AddTrip extends AppCompatActivity implements
@@ -47,15 +49,12 @@ public class AddTrip extends AppCompatActivity implements
         HomeContract.AddTripView {
 
     Calendar c = Calendar.getInstance();
-
     HomeContract.HomePresenter addTripPresenter;
 
     @BindView(R.id.name)
     TextInputEditText name;
-    @BindView(R.id.epoint)
-    TextInputEditText endPoint;
-    @BindView(R.id.spoint)
-    TextInputEditText startPoint;
+//    @BindView(R.id.epoint)
+//    TextInputEditText endPoint;
     @BindView(R.id.date)
     TextInputEditText date;
     @BindView(R.id.time)
@@ -68,30 +67,23 @@ public class AddTrip extends AppCompatActivity implements
    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @OnClick(R.id.add) void addTrip(View view){
         String tripName = name.getText().toString();
-        String tripStartPoint = startPoint.getText().toString();
-        String tripEndPoint = endPoint.getText().toString();
+       // String tripStartPoint = startPoint.getText().toString();
+//        String tripEndPoint = endPoint.getText().toString();
         String tripDate = date.getText().toString();
         String tripTime = time.getText().toString();
         String tripNotes = notes.getText().toString();
         String tripType  = String.valueOf(isRoundTrip());
-        String tripStatus = Uitiles.STATUS_UPCOMING;
+        String tripStatus = "Upcoming";
         //alarm manager
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(AddTrip.this,AlertReceiver.class);
         PendingIntent pi =  PendingIntent.getBroadcast(AddTrip.this,1,intent,0);
         alarmManager.setExact(AlarmManager.RTC_WAKEUP,c.getTimeInMillis(),pi);
         //alarmManager.cancel(pi);
-        //  name - startLoc -  endLoc -  date -  time -  type -  notes
-        Trip trip = new Trip(tripName, tripStartPoint, tripEndPoint, tripDate, tripTime,
-                tripType, tripNotes, tripStatus, SAVED_ONLINE);
-        if (checkInternetState(this)) {
-            trip.setIsSavedOnline(SAVED_ONLINE);
-            addTripPresenter.onSaveTrip(trip);
-        } else {
-            trip.setIsSavedOnline(SAVED_OFFLINE);
-            // TODO 1- Salah add to room DB
-        }
-  
+      //  name - startLoc -  endLoc -  date -  time -  type -  notes
+        Trip trip = new Trip(tripName, "tripStartPoint", "tripEndPoint", tripDate, tripTime,
+                tripType, tripNotes, tripStatus);
+        addTripPresenter.onSaveTrip(trip);
         finish();
     }
 
@@ -128,7 +120,49 @@ public class AddTrip extends AppCompatActivity implements
         ButterKnife.bind(this);
 
         addTripPresenter = new HomePresenterImpl(new FirebaseModelImpl(), this);
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), "AIzaSyDuifm35ZNF7OOG7exwhOrda3mb1H8qFnA");
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment1 = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.spoint);
 
+        AutocompleteSupportFragment autocompleteFragment2 = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.epoint);
+// Set up a PlaceSelectionListener to handle the response.
+        // Specify the types of place data to return.
+        autocompleteFragment1.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME));
+        autocompleteFragment2.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME));
+        autocompleteFragment1.setCountry("EG");
+        autocompleteFragment2.setCountry("EG");
+        autocompleteFragment1.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Hossam Get info about the selected place.
+                Toast.makeText(getApplicationContext()," "+place.getLatLng(),Toast.LENGTH_SHORT).show();
+                Log.i("TAG", "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: " + status);
+            }
+        });
+
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Hossam Get info about the selected place.
+                Toast.makeText(getApplicationContext()," "+place.getName(),Toast.LENGTH_SHORT).show();
+                Log.i("TAG", "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: " + status);
+            }
+        });
 
     }
 
@@ -137,10 +171,8 @@ public class AddTrip extends AppCompatActivity implements
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         Log.i("TAG", "onTimeSet: ");
         time.setText(hourOfDay + ":" + minute);
-
         c.set(Calendar.HOUR_OF_DAY,hourOfDay);
         c.set(Calendar.MINUTE,minute);
-
     }
 
     @Override
@@ -150,17 +182,15 @@ public class AddTrip extends AppCompatActivity implements
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         date.setText(dayOfMonth + "/" + month + "/" + year);
-
         c.set(Calendar.YEAR,year);
         c.set(Calendar.MONTH,month);
         c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-
     }
 
 
     @Override
     public void onTripSaveSuccess(String state) {
-       // Toast.makeText(this, "Trip Added Successfully", Toast.LENGTH_SHORT).show();
+
     }
 
 }
